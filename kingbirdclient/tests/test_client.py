91305a0ebf0f1a1c578d21293dd0803bd1d02716
Dimitri Mazmanov <dimitri.mazmanov@ericsson.com>
2016-07-12 14:48:37 +0200

    Add common layer for the client
     - Added global CLI arguments
     - Added translation
     - Added exceptions
     - Added basic client skeleton
     - Added OpenStack common modules
     - Added Kingbird http client
    
    This commit adds common routines that will be used by the client.
    The modules are generic and are used by other openstack clients
    
    Change-Id: I54737e2a30f94fe935794f619bfbac5889b0a1b2

---
+++ kingbirdclient/tests/test_client.py
@@ -0,0 +1,215 @@
+# Copyright 2015 - Huawei Technologies Co., Ltd.
+# Copyright 2016 - StackStorm, Inc.
+# Copyright 2016 - Ericsson AB.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import os
+import tempfile
+import uuid
+
+import mock
+import testtools
+
+import osprofiler.profiler
+
+from kingbirdclient.api import client
+
+AUTH_HTTP_URL = 'http://localhost:35357/v3'
+AUTH_HTTPS_URL = AUTH_HTTP_URL.replace('http', 'https')
+KINGBIRD_HTTP_URL = 'http://localhost:8118/v1.0'
+KINGBIRD_HTTPS_URL = KINGBIRD_HTTP_URL.replace('http', 'https')
+PROFILER_HMAC_KEY = 'SECRET_HMAC_KEY'
+
+
+class BaseClientTests(testtools.TestCase):
+    @mock.patch('keystoneclient.v3.client.Client')
+    @mock.patch('kingbirdclient.api.httpclient.HTTPClient')
+    def test_kingbird_url_default(self, mock, keystone_client_mock):
+        keystone_client_instance = keystone_client_mock.return_value
+        keystone_client_instance.auth_token = str(uuid.uuid4())
+        keystone_client_instance.project_id = str(uuid.uuid4())
+        keystone_client_instance.user_id = str(uuid.uuid4())
+
+        expected_args = (
+            KINGBIRD_HTTP_URL,
+            keystone_client_instance.auth_token,
+            keystone_client_instance.project_id,
+            keystone_client_instance.user_id
+        )
+
+        expected_kwargs = {
+            'cacert': None,
+            'insecure': False
+        }
+
+        client.client(
+            username='kingbird',
+            project_name='kingbird',
+            auth_url=AUTH_HTTP_URL
+        )
+
+        self.assertTrue(mock.called)
+        self.assertEqual(mock.call_args[0], expected_args)
+        self.assertDictEqual(mock.call_args[1], expected_kwargs)
+
+    @mock.patch('keystoneclient.v3.client.Client')
+    @mock.patch('kingbirdclient.api.httpclient.HTTPClient')
+    def test_kingbird_url_https_insecure(self, mock, keystone_client_mock):
+        keystone_client_instance = keystone_client_mock.return_value
+        keystone_client_instance.auth_token = str(uuid.uuid4())
+        keystone_client_instance.project_id = str(uuid.uuid4())
+        keystone_client_instance.user_id = str(uuid.uuid4())
+
+        expected_args = (
+            KINGBIRD_HTTPS_URL,
+            keystone_client_instance.auth_token,
+            keystone_client_instance.project_id,
+            keystone_client_instance.user_id
+        )
+
+        expected_kwargs = {
+            'cacert': None,
+            'insecure': True
+        }
+
+        client.client(
+            kingbird_url=KINGBIRD_HTTPS_URL,
+            username='kingbird',
+            project_name='kingbird',
+            auth_url=AUTH_HTTP_URL,
+            cacert=None,
+            insecure=True
+        )
+
+        self.assertTrue(mock.called)
+        self.assertEqual(mock.call_args[0], expected_args)
+        self.assertDictEqual(mock.call_args[1], expected_kwargs)
+
+    @mock.patch('keystoneclient.v3.client.Client')
+    @mock.patch('kingbirdclient.api.httpclient.HTTPClient')
+    def test_kingbird_url_https_secure(self, mock, keystone_client_mock):
+        fd, path = tempfile.mkstemp(suffix='.pem')
+
+        keystone_client_instance = keystone_client_mock.return_value
+        keystone_client_instance.auth_token = str(uuid.uuid4())
+        keystone_client_instance.project_id = str(uuid.uuid4())
+        keystone_client_instance.user_id = str(uuid.uuid4())
+
+        expected_args = (
+            KINGBIRD_HTTPS_URL,
+            keystone_client_instance.auth_token,
+            keystone_client_instance.project_id,
+            keystone_client_instance.user_id
+        )
+
+        expected_kwargs = {
+            'cacert': path,
+            'insecure': False
+        }
+
+        try:
+            client.client(
+                kingbird_url=KINGBIRD_HTTPS_URL,
+                username='kingbird',
+                project_name='kingbird',
+                auth_url=AUTH_HTTP_URL,
+                cacert=path,
+                insecure=False
+            )
+        finally:
+            os.close(fd)
+            os.unlink(path)
+
+        self.assertTrue(mock.called)
+        self.assertEqual(mock.call_args[0], expected_args)
+        self.assertDictEqual(mock.call_args[1], expected_kwargs)
+
+    @mock.patch('keystoneclient.v3.client.Client')
+    def test_kingbird_url_https_bad_cacert(self, keystone_client_mock):
+        keystone_client_instance = keystone_client_mock.return_value
+        keystone_client_instance.auth_token = str(uuid.uuid4())
+        keystone_client_instance.project_id = str(uuid.uuid4())
+        keystone_client_instance.user_id = str(uuid.uuid4())
+
+        self.assertRaises(
+            ValueError,
+            client.client,
+            kingbird_url=KINGBIRD_HTTPS_URL,
+            username='kingbird',
+            project_name='kingbird',
+            auth_url=AUTH_HTTP_URL,
+            cacert='/path/to/foobar',
+            insecure=False
+        )
+
+    @mock.patch('logging.Logger.warning')
+    @mock.patch('keystoneclient.v3.client.Client')
+    def test_kingbird_url_https_bad_insecure(self, keystone_client_mock,
+                                             log_warning_mock):
+        fd, path = tempfile.mkstemp(suffix='.pem')
+
+        keystone_client_instance = keystone_client_mock.return_value
+        keystone_client_instance.auth_token = str(uuid.uuid4())
+        keystone_client_instance.project_id = str(uuid.uuid4())
+        keystone_client_instance.user_id = str(uuid.uuid4())
+
+        try:
+            client.client(
+                kingbird_url=KINGBIRD_HTTPS_URL,
+                username='kingbird',
+                project_name='kingbird',
+                auth_url=AUTH_HTTP_URL,
+                cacert=path,
+                insecure=True
+            )
+        finally:
+            os.close(fd)
+            os.unlink(path)
+
+        self.assertTrue(log_warning_mock.called)
+
+    @mock.patch('keystoneclient.v3.client.Client')
+    @mock.patch('kingbirdclient.api.httpclient.HTTPClient')
+    def test_kingbird_profile_enabled(self, mock, keystone_client_mock):
+        keystone_client_instance = keystone_client_mock.return_value
+        keystone_client_instance.auth_token = str(uuid.uuid4())
+        keystone_client_instance.project_id = str(uuid.uuid4())
+        keystone_client_instance.user_id = str(uuid.uuid4())
+
+        expected_args = (
+            KINGBIRD_HTTP_URL,
+            keystone_client_instance.auth_token,
+            keystone_client_instance.project_id,
+            keystone_client_instance.user_id
+        )
+
+        expected_kwargs = {
+            'cacert': None,
+            'insecure': False
+        }
+
+        client.client(
+            username='kingbird',
+            project_name='kingbird',
+            auth_url=AUTH_HTTP_URL,
+            profile=PROFILER_HMAC_KEY
+        )
+
+        self.assertTrue(mock.called)
+        self.assertEqual(mock.call_args[0], expected_args)
+        self.assertDictEqual(mock.call_args[1], expected_kwargs)
+
+        profiler = osprofiler.profiler.get()
+
+        self.assertEqual(profiler.hmac_key, PROFILER_HMAC_KEY)
---
+++ kingbirdclient/tests/test_httpclient.py
@@ -0,0 +1,296 @@
+# Copyright 2016 - StackStorm, Inc.
+# Copyright 2016 - Ericsson AB.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import copy
+import uuid
+
+import mock
+import requests
+import testtools
+
+from osprofiler import _utils as osprofiler_utils
+import osprofiler.profiler
+
+from kingbirdclient.api import httpclient
+
+API_BASE_URL = 'http://localhost:8118/v1.0'
+API_URL = '/os-quota-sets'
+
+EXPECTED_URL = API_BASE_URL + API_URL
+
+AUTH_TOKEN = str(uuid.uuid4())
+PROJECT_ID = str(uuid.uuid4())
+USER_ID = str(uuid.uuid4())
+PROFILER_HMAC_KEY = 'SECRET_HMAC_KEY'
+PROFILER_TRACE_ID = str(uuid.uuid4())
+
+EXPECTED_AUTH_HEADERS = {
+    'x-auth-token': AUTH_TOKEN,
+    'X-Project-Id': PROJECT_ID,
+    'X-User-Id': USER_ID
+}
+
+EXPECTED_REQ_OPTIONS = {
+    'headers': EXPECTED_AUTH_HEADERS
+}
+
+EXPECTED_BODY = {
+    'k1': 'abc',
+    'k2': 123,
+    'k3': True
+}
+
+
+class FakeRequest(object):
+    def __init__(self, method):
+        self.method = method
+
+
+class FakeResponse(object):
+    def __init__(self, method, url, status_code):
+        self.request = FakeRequest(method)
+        self.url = url
+        self.status_code = status_code
+
+
+class HTTPClientTest(testtools.TestCase):
+    def setUp(self):
+        super(HTTPClientTest, self).setUp()
+        osprofiler.profiler.init(None)
+        self.client = httpclient.HTTPClient(
+            API_BASE_URL,
+            AUTH_TOKEN,
+            PROJECT_ID,
+            USER_ID
+        )
+
+    @mock.patch.object(
+        requests,
+        'get',
+        mock.MagicMock(return_value=FakeResponse('get', EXPECTED_URL, 200))
+    )
+    def test_get_request_options(self):
+        self.client.get(API_URL)
+
+        requests.get.assert_called_with(
+            EXPECTED_URL,
+            **EXPECTED_REQ_OPTIONS
+        )
+
+    @mock.patch.object(
+        requests,
+        'get',
+        mock.MagicMock(return_value=FakeResponse('get', EXPECTED_URL, 200))
+    )
+    def test_get_request_options_with_headers_for_get(self):
+        headers = {'foo': 'bar'}
+
+        self.client.get(API_URL, headers=headers)
+
+        expected_options = copy.deepcopy(EXPECTED_REQ_OPTIONS)
+        expected_options['headers'].update(headers)
+
+        requests.get.assert_called_with(
+            EXPECTED_URL,
+            **expected_options
+        )
+
+    @mock.patch.object(
+        osprofiler.profiler._Profiler,
+        'get_base_id',
+        mock.MagicMock(return_value=PROFILER_TRACE_ID)
+    )
+    @mock.patch.object(
+        osprofiler.profiler._Profiler,
+        'get_id',
+        mock.MagicMock(return_value=PROFILER_TRACE_ID)
+    )
+    @mock.patch.object(
+        requests,
+        'get',
+        mock.MagicMock(return_value=FakeResponse('get', EXPECTED_URL, 200))
+    )
+    def test_get_request_options_with_profile_enabled(self):
+        osprofiler.profiler.init(PROFILER_HMAC_KEY)
+
+        data = {'base_id': PROFILER_TRACE_ID, 'parent_id': PROFILER_TRACE_ID}
+        signed_data = osprofiler_utils.signed_pack(data, PROFILER_HMAC_KEY)
+
+        headers = {
+            'X-Trace-Info': signed_data[0],
+            'X-Trace-HMAC': signed_data[1]
+        }
+
+        self.client.get(API_URL)
+
+        expected_options = copy.deepcopy(EXPECTED_REQ_OPTIONS)
+        expected_options['headers'].update(headers)
+
+        requests.get.assert_called_with(
+            EXPECTED_URL,
+            **expected_options
+        )
+
+    @mock.patch.object(
+        requests,
+        'post',
+        mock.MagicMock(return_value=FakeResponse('post', EXPECTED_URL, 201))
+    )
+    def test_get_request_options_with_headers_for_post(self):
+        headers = {'foo': 'bar'}
+
+        self.client.post(API_URL, EXPECTED_BODY, headers=headers)
+
+        expected_options = copy.deepcopy(EXPECTED_REQ_OPTIONS)
+        expected_options['headers'].update(headers)
+        expected_options['headers']['content-type'] = 'application/json'
+
+        requests.post.assert_called_with(
+            EXPECTED_URL,
+            EXPECTED_BODY,
+            **expected_options
+        )
+
+    @mock.patch.object(
+        requests,
+        'put',
+        mock.MagicMock(return_value=FakeResponse('put', EXPECTED_URL, 200))
+    )
+    def test_get_request_options_with_headers_for_put(self):
+        headers = {'foo': 'bar'}
+
+        self.client.put(API_URL, EXPECTED_BODY, headers=headers)
+
+        expected_options = copy.deepcopy(EXPECTED_REQ_OPTIONS)
+        expected_options['headers'].update(headers)
+        expected_options['headers']['content-type'] = 'application/json'
+
+        requests.put.assert_called_with(
+            EXPECTED_URL,
+            EXPECTED_BODY,
+            **expected_options
+        )
+
+    @mock.patch.object(
+        requests,
+        'delete',
+        mock.MagicMock(return_value=FakeResponse('delete', EXPECTED_URL, 200))
+    )
+    def test_get_request_options_with_headers_for_delete(self):
+        headers = {'foo': 'bar'}
+
+        self.client.delete(API_URL, headers=headers)
+
+        expected_options = copy.deepcopy(EXPECTED_REQ_OPTIONS)
+        expected_options['headers'].update(headers)
+
+        requests.delete.assert_called_with(
+            EXPECTED_URL,
+            **expected_options
+        )
+
+    @mock.patch.object(
+        httpclient.HTTPClient,
+        '_get_request_options',
+        mock.MagicMock(return_value=copy.deepcopy(EXPECTED_REQ_OPTIONS))
+    )
+    @mock.patch.object(
+        requests,
+        'get',
+        mock.MagicMock(return_value=FakeResponse('get', EXPECTED_URL, 200))
+    )
+    def test_http_get(self):
+        self.client.get(API_URL)
+
+        httpclient.HTTPClient._get_request_options.assert_called_with(
+            'get',
+            None
+        )
+
+        requests.get.assert_called_with(
+            EXPECTED_URL,
+            **EXPECTED_REQ_OPTIONS
+        )
+
+    @mock.patch.object(
+        httpclient.HTTPClient,
+        '_get_request_options',
+        mock.MagicMock(return_value=copy.deepcopy(EXPECTED_REQ_OPTIONS))
+    )
+    @mock.patch.object(
+        requests,
+        'post',
+        mock.MagicMock(return_value=FakeResponse('post', EXPECTED_URL, 201))
+    )
+    def test_http_post(self):
+        self.client.post(API_URL, EXPECTED_BODY)
+
+        httpclient.HTTPClient._get_request_options.assert_called_with(
+            'post',
+            None
+        )
+
+        requests.post.assert_called_with(
+            EXPECTED_URL,
+            EXPECTED_BODY,
+            **EXPECTED_REQ_OPTIONS
+        )
+
+    @mock.patch.object(
+        httpclient.HTTPClient,
+        '_get_request_options',
+        mock.MagicMock(return_value=copy.deepcopy(EXPECTED_REQ_OPTIONS))
+    )
+    @mock.patch.object(
+        requests,
+        'put',
+        mock.MagicMock(return_value=FakeResponse('put', EXPECTED_URL, 200))
+    )
+    def test_http_put(self):
+        self.client.put(API_URL, EXPECTED_BODY)
+
+        httpclient.HTTPClient._get_request_options.assert_called_with(
+            'put',
+            None
+        )
+
+        requests.put.assert_called_with(
+            EXPECTED_URL,
+            EXPECTED_BODY,
+            **EXPECTED_REQ_OPTIONS
+        )
+
+    @mock.patch.object(
+        httpclient.HTTPClient,
+        '_get_request_options',
+        mock.MagicMock(return_value=copy.deepcopy(EXPECTED_REQ_OPTIONS))
+    )
+    @mock.patch.object(
+        requests,
+        'delete',
+        mock.MagicMock(return_value=FakeResponse('delete', EXPECTED_URL, 200))
+    )
+    def test_http_delete(self):
+        self.client.delete(API_URL)
+
+        httpclient.HTTPClient._get_request_options.assert_called_with(
+            'delete',
+            None
+        )
+
+        requests.delete.assert_called_with(
+            EXPECTED_URL,
+            **EXPECTED_REQ_OPTIONS
+        )
