1d131991cfafa82ecb5fec137327a1bb9be55697
Goutham Pratapa <pratapagoutham@gmail.com>
2017-02-20 18:59:30 +0530

    Implement Resource_sync commands for KB
    "sync create" Sync Resources from One region to other.
    "sync list"  List Sync Jobs.
    "sync show"  List the details of a Sync Job.
    "sync delete" Delete Sync Job(s) details from the database.
    Add test-cases for the same.
    
    Python-kingbirdclient is now a part of openstack/requirements.
    https://review.openstack.org/#/c/428793/.
    So updated tox.ini to pick requirements from openstack/requirements
    directly rather than using install _ commands.
    Also added tox_install.sh script
    
    Closes-Bug: #1666453
    
    Change-Id: I587070a7175ea0651def5630c2d9890a175feb0a

---
+++ kingbirdclient/api/v1/sync_manager.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2017 Ericsson AB.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from kingbirdclient.api import base
+
+
+class Resource(base.Resource):
+    resource_name = 'os-sync'
+
+    def __init__(self, manager, status, created_at, updated_at=None,
+                 resource_type=None, target_region=None,
+                 source_region=None, id=None, resource_name=None,):
+        self.manager = manager
+        self.id = id
+        self.source_region = source_region
+        self.target_region = target_region
+        self.status = status
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.resource_name = resource_name
+        self.resource_type = resource_type
+
+
+class sync_manager(base.ResourceManager):
+    resource_class = Resource
+
+    def sync_resources(self, **kwargs):
+        tenant = self.http_client.project_id
+        data = dict()
+        data['resource_set'] = kwargs
+        url = '/%s/os-sync/' % tenant
+        return self.resource_sync_create(url, data)
+
+    def list_sync_jobs(self):
+        tenant = self.http_client.project_id
+        url = '/%s/os-sync/' % tenant
+        return self._resource_sync_list(url)
+
+    def sync_job_detail(self, job_id):
+        tenant = self.http_client.project_id
+        url = '/%s/os-sync/%s' % (tenant, job_id)
+        return self._resource_sync_detail(url)
+
+    def delete_sync_job(self, job_id):
+        tenant = self.http_client.project_id
+        url = '/%s/os-sync/%s' % (tenant, job_id)
+        return self._delete(url)
---
+++ kingbirdclient/commands/v1/sync_manager.py
@@ -0,0 +1,201 @@
+# Copyright (c) 2017 Ericsson AB.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from osc_lib.command import command
+
+from kingbirdclient.commands.v1 import base
+from kingbirdclient import exceptions
+
+
+def format(resources=None):
+    columns = (
+        'ID',
+        'STATUS',
+        'CREATED_AT',
+        'UPDATED_AT',
+    )
+
+    if resources:
+        data = (
+            resources.id,
+            resources.status,
+            resources.created_at,
+            resources.updated_at,
+        )
+
+    else:
+        data = (tuple('<none>' for _ in range(len(columns))),)
+
+    return columns, data
+
+
+def detail_format(resources=None):
+    columns = (
+        'RESOURCE',
+        'SOURCE_REGION',
+        'TARGET_REGION',
+        'RESOURCE_TYPE',
+        'STATUS',
+        'CREATED_AT',
+        'UPDATED_AT',
+    )
+
+    if resources:
+        data = (
+            resources.resource_name,
+            resources.source_region,
+            resources.target_region,
+            resources.resource_type,
+            resources.status,
+            resources.created_at,
+            resources.updated_at,
+        )
+
+    else:
+        data = (tuple('<none>' for _ in range(len(columns))),)
+
+    return columns, data
+
+
+def sync_format(resources=None):
+    columns = (
+        'ID',
+        'STATUS',
+        'CREATED_AT',
+    )
+
+    if resources:
+        data = (
+            resources.id,
+            resources.status,
+            resources.created_at,
+        )
+
+    else:
+        data = (tuple('<none>' for _ in range(len(columns))),)
+
+    return columns, data
+
+
+class ResourceSync(base.KingbirdLister):
+    """Sync Resources from One region to other."""
+
+    def _get_format_function(self):
+        return sync_format
+
+    def get_parser(self, parsed_args):
+        parser = super(ResourceSync, self).get_parser(parsed_args)
+
+        parser.add_argument(
+            '--source',
+            required=True,
+            help='Source Region from which resources have to be synced.'
+        )
+
+        parser.add_argument(
+            '--target',
+            action='append',
+            required=True,
+            help='Target Region to which resources have to be synced.'
+        )
+
+        parser.add_argument(
+            '--resource_type',
+            required=True,
+            help='Type of the resource to be synced.'
+        )
+
+        parser.add_argument(
+            '--resources',
+            action='append',
+            required=True,
+            help='Identifier of the resource',
+        )
+
+        parser.add_argument(
+            '--force',
+            action='store_true',
+            help='Forcefully sync the resources to target_regions'
+        )
+
+        return parser
+
+    def _get_resources(self, parsed_args):
+        kingbird_client = self.app.client_manager.sync_engine
+        kwargs = dict()
+        kwargs['resource_type'] = parsed_args.resource_type
+        kwargs['force'] = str(parsed_args.force)
+        kwargs['resources'] = parsed_args.resources
+        kwargs['source'] = parsed_args.source
+        kwargs['target'] = parsed_args.target
+        return kingbird_client.sync_manager.sync_resources(**kwargs)
+
+
+class SyncList(base.KingbirdLister):
+    """List Sync Jobs."""
+
+    def _get_format_function(self):
+        return format
+
+    def _get_resources(self, parsed_args):
+        kingbird_client = self.app.client_manager.sync_engine
+        return kingbird_client.sync_manager.list_sync_jobs()
+
+
+class SyncShow(base.KingbirdLister):
+    """List the details of a Sync Job."""
+
+    def _get_format_function(self):
+        return detail_format
+
+    def get_parser(self, parsed_args):
+        parser = super(SyncShow, self).get_parser(parsed_args)
+
+        parser.add_argument(
+            'job_id',
+            help='ID of Job to view the details.'
+        )
+
+        return parser
+
+    def _get_resources(self, parsed_args):
+        job_id = parsed_args.job_id
+        kingbird_client = self.app.client_manager.sync_engine
+        return kingbird_client.sync_manager.sync_job_detail(job_id)
+
+
+class SyncDelete(command.Command):
+    """Delete Sync Job(s) details from the database."""
+
+    def get_parser(self, prog_name):
+        parser = super(SyncDelete, self).get_parser(prog_name)
+
+        parser.add_argument(
+            'job_id',
+            nargs="+",
+            help='ID of the job to delete entries in database.'
+        )
+
+        return parser
+
+    def take_action(self, parsed_args):
+        jobs = parsed_args.job_id
+        kingbird_client = self.app.client_manager.sync_engine
+        for job in jobs:
+            try:
+                kingbird_client.sync_manager.delete_sync_job(job)
+            except Exception as e:
+                print (e)
+                error_msg = "Unable to delete the entries of %s" % (job)
+                raise exceptions.KingbirdClientException(error_msg)
